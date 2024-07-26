31dbb7e15954c9109b55b3836814914a1207c0aa
Joseph Van Drunen <josephvandrunen@gmail.com>
2024-07-26 15:15:05 -0400

    fixup

---
+++ lambdas/lib/__mocks__/env-paths.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-undef
+const envPaths = jest.fn(() => ({
+  data: 'mock',
+  config: 'mock',
+  cache: 'mock',
+  log: 'mock',
+  temp: 'mock',
+}));
+
+module.exports = envPaths;
