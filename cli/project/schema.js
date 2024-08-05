063a8a44394676c3c06cf2e05495d142b9148f74
Joseph Van Drunen <josephvandrunen@gmail.com>
2024-08-05 10:22:19 -0400

    fixups

--- cli/project/schema.js
+++ cli/project/schema.js
@@ -32,56 +32,58 @@ const modelSchema = joi.object({
   }),
 });
 
-const sourceSchema = joi.object({
-  name: joi.string().required(),
-  description: joi.string().required(),
-  format: joi.valid(
-    'csv',
-    'json',
-    'orc',
-    'parquet',
-    'cloudtrail',
-    'cloudfront'
-  ),
-  custom_format: joi.alternatives().conditional('format', {
-    is: joi.any().empty(),
-    then: joi
-      .object({
-        input_format: joi.string().required(),
-        output_format: joi.string().required(),
-        serde_info: joi.object({
-          serialization_library: joi.string().required(),
-          parameters: joi.object().pattern(joi.string(), joi.string()),
-        }),
-        compressed: joi.boolean().default(true),
-        stored_as_sub_directories: joi.boolean().default(true),
-        number_of_buckets: joi.number().min(0).default(0),
-      })
-      .required(),
-  }),
-  input_location: joi.object({
-    storage: joi.string().required(),
-    path: joi
+const sourceSchema = joi
+  .object({
+    name: joi.string().required(),
+    description: joi.string().required(),
+    format: joi
       .string()
-      .required()
-      .pattern(/^s3:\/\/([A-Za-z0-9-_./])+\/$/),
-  }),
-  columns: joi.array().items(
-    joi.object({
-      name: joi.string().required(),
-      type: joi.string().required(),
-    })
-  ),
-  partitions: joi.array().items(
-    joi.object({
-      name: joi.string().required(),
-      type: joi.string().required(),
-    })
-  ),
-  service_level_agreement: joi.object({
-    freshness: joi.number().required().min(5).max(10080),
-  }),
-});
+      .valid('csv', 'json', 'orc', 'parquet', 'cloudtrail', 'cloudfront')
+      .when('custom_format', {
+        is: joi.any().exist(),
+        then: joi.forbidden(),
+      }),
+    custom_format: joi.alternatives().conditional('format', {
+      is: joi.any().exist(),
+      then: joi.forbidden(),
+      otherwise: joi
+        .object({
+          input_format: joi.string().required(),
+          output_format: joi.string().required(),
+          serde_info: joi.object({
+            serialization_library: joi.string().required(),
+            parameters: joi.object().pattern(joi.string(), joi.string()),
+          }),
+          compressed: joi.boolean().default(true),
+          stored_as_sub_directories: joi.boolean().default(true),
+          number_of_buckets: joi.number().min(0).default(0),
+        })
+        .required(),
+    }),
+    input_location: joi.object({
+      storage: joi.string().required(),
+      path: joi
+        .string()
+        .required()
+        .pattern(/^s3:\/\/([A-Za-z0-9-_./])+\/$/),
+    }),
+    columns: joi.array().items(
+      joi.object({
+        name: joi.string().required(),
+        type: joi.string().required(),
+      })
+    ),
+    partitions: joi.array().items(
+      joi.object({
+        name: joi.string().required(),
+        type: joi.string().required(),
+      })
+    ),
+    service_level_agreement: joi.object({
+      freshness: joi.number().required().min(5).max(10080),
+    }),
+  })
+  .xor('format', 'custom_format');
 
 const projectSchema = joi.object({
   name: joi.string().required(),
