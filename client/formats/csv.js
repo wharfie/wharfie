e2f533bf3f22665a2194b185f7aee90f8dfe7046
Joseph Van Drunen <josephvandrunen@gmail.com>
2024-08-13 09:51:00 -0400

    fix csv format

--- client/formats/csv.js
+++ client/formats/csv.js
@@ -15,8 +15,8 @@ module.exports = ({
   StorageDescriptor: {
     Location,
     Columns,
-    InputFormat: 'org.apache.hadoop.hive.ql.io.SymlinkTextInputFormat',
-    OutputFormat: 'org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat',
+    InputFormat: 'org.apache.hadoop.mapred.TextInputFormat',
+    OutputFormat: 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat',
     SerdeInfo: {
       SerializationLibrary: 'org.apache.hadoop.hive.serde2.OpenCSVSerde',
       Parameters: { separatorChar: ',' },
